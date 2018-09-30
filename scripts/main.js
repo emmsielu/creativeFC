bdd969b2891a7c23431df06e42c764281afd8c0a
Emma <emmalucybevan@gmail.com>
2018-09-30 23:27:34 +0100

    adding few more list items an correctig the sentence. adding poem text to page.

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@
 	var buttons = document.querySelectorAll('.button');
 
 	//var list = document.querySelector('.selection');
-	var resultsText = document.getElementsByClassName(".poemText");
+	var resultsText = document.getElementById("poemText");
 	
 
 	/*function fetchCardLists(){
@@ -60,44 +60,41 @@
 
 		var person = chooseCard('peopleJobs');
 		var superHeroVillain = chooseCard('superheroesVillains');
+		var superHeroVillainTwo = chooseCard('superheroesVillains');
 		var colour = chooseCard('colours');
 		var animal = chooseCard('animals');
 		var action = chooseCard('actions');
 		var actionTwo = chooseCard('actions');
 		var locationSituation = chooseCard('locationsSituations');
 		var locationSituationTwo = chooseCard('locationsSituations');
-		// var weather = chooseCard('weather');
-		// var foodAndDrink = chooseCard('foodDrink');
-		// var bodyParts = chooseCard('bodyParts');
-		// var transport = chooseCard('transport');
+		var weather = chooseCard('weather');
+		var foodAndDrink = chooseCard('foodDrink');
+		var bodyParts = chooseCard('bodyParts');
+		var transport = chooseCard('transport');
 		var accessory = chooseCard('accessories');
 		var actionDoneToThem = chooseCard('receivedAction');
+		var object = chooseCard('objects');
 
 		var poem;
 		var buttonName = event.target.name;
 
 		if(buttonName === 'combo1'){
 
-			poem = `${person} and a
-			${colour} ${animal} are ${action},
-			${locationSituation} when they are ${actionDoneToThem} 
-			by ${superHeroVillain}`;
+			poem = `${person} and a ${colour} ${animal} are ${action} ${locationSituation}, when suddenly, they are ${actionDoneToThem} by ${superHeroVillain} with an overly large ${foodAndDrink}`;
 
 
 		}else if(buttonName === 'combo2'){
 
 			//poem = 'combo 2 poem';
-			poem = `${person} is ${action} with ${superHeroVillain}, while ${actionTwo} ${locationSituation}.
-			Meanwhile behind them, a ${colour} ${animal} wearing ${accessory}, is ${locationSituationTwo}`;
+			poem = `It is ${weather}, but ${person} is ${action} with ${superHeroVillain}, and secretely wanting to be ${actionTwo} with ${superHeroVillainTwo}. Meanwhile, behind them a ${colour} ${animal} wearing ${accessory}, is shaking their ${bodyParts}`;
 
 		}else if(buttonName === 'combo3'){
 
-			poem = `a ${colour} ${animal} in ${accessory} is ${action} while ${person} is ${actionTwo} by ${superHeroVillain} ${locationSituation}`;
+			poem = `a ${colour} ${animal} in ${accessory} is ${action}, while not too far away ${person} is ${actionDoneToThem} by ${superHeroVillain} with ${object}`;
 
 		}
-
-		resultsText.innerHTML = poem;
 		console.log(poem);
+		resultsText.innerHTML = poem;
 		
 	}
 
